ff094c8f64ce3622676827bb986b92240dc0c1d9
Yein Lee <zxth47e@gmail.com>
2025-06-15 01:07:44 +0900

    feat: JSX에서 동적인 표현식 사용하는 방법을 보여주기 위한 예제

--- lectures/udemy-react-complete-guide/01-react-essentials/src/App.jsx
+++ lectures/udemy-react-complete-guide/01-react-essentials/src/App.jsx
@@ -1,25 +1,35 @@
+import reactImg from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Chore"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
-    return (
-        <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-                Fundamental React concepts you will need for almost any app you are
-                going to build!
-            </p>
-        </header>
-    );
+  const description = reactDescriptions[getRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
 }
 
 function App() {
-    return (
-        <div>
-            <main>
-                <Header />
-                <h2>Time to get started!</h2>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <main>
+        <Header />
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
 }
 
 export default App;
